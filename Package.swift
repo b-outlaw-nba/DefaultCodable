1f1499b3c773758825ea9219c2d6e370207ae580
b-outlaw-nba <129885160+b-outlaw-nba@users.noreply.github.com>
2024-06-27 10:17:35 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,13 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "DefaultCodable",
-    products: [
-        .library(name: "DefaultCodable", targets: ["DefaultCodable"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(name: "DefaultCodable", dependencies: []),
-        .testTarget(name: "DefaultCodableTests", dependencies: ["DefaultCodable"]),
-    ]
+  name: "DefaultCodable",
+  defaultLocalization: "en",
+  platforms: [
+    .iOS(.v15),
+    .macOS(.v14),
+    .tvOS(.v15),
+    .visionOS(.v1),
+    .watchOS(.v10)
+  ],
+  products: [
+    .library(name: "DefaultCodable", targets: ["DefaultCodable"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(name: "DefaultCodable", dependencies: []),
+    .testTarget(name: "DefaultCodableTests", dependencies: ["DefaultCodable"]),
+  ]
 )
